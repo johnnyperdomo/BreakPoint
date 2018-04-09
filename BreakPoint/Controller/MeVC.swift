ba0f09bfa809745809cbe54979acb8846a9197f4
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-04-09 19:59:55 -0400

    Build meVC interface

---
+++ BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Johnny Perdomo on 4/9/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func signOutBtnPressed(_ sender: Any) { //used to sign out the app
+    }
+    
+    
+    
+}
