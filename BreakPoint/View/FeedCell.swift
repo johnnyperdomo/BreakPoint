bf0fe7dee7b7e7c157aabbf3def8c2bdba01d6db
Johnny Perdomo <johnnyperdomo7@gmail.com>
2018-04-12 20:45:00 -0400

    Build FeedVC

---
+++ BreakPoint/View/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  BreakPoint
+//
+//  Created by Johnny Perdomo on 4/12/18.
+//  Copyright © 2018 Johnny Perdomo. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var messageContentLbl: UILabel!
+    
+    func configureCell(profileImage: UIImage, email: String, messageContent: String) {
+        self.profileImg.image = profileImage
+        self.emailLbl.text = email
+        self.messageContentLbl.text = messageContent
+    }
+    
+    
+}
